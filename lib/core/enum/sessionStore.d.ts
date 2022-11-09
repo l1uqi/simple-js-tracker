01c85f5e9aad92483d54539dc4ec19c172539b86
Chyi <70128222+l1uqi@users.noreply.github.com>
2022-11-09 21:27:26 +0800

    remove

--- lib/core/enum/sessionStore.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum sessionStoreEnum {
-    OPSIONS = "OPSIONS",
-    PRE_TIMESTAMP = "PRE_TIMESTAMP",
-    PAGE_PERFORMANCE = "PAGE_PERFORMANCE",
-    PAGE_INFO = "PAGE_INFO",
-    UUID = "UUID"
-}
--- lib/core/utils/basic.d.ts
+++
@@ -1,8 +0,0 @@
-import { ICustomOptions } from "../types/index";
-export declare const filterParams: (data: ICustomOptions) => ICustomOptions;
-export declare const getheatMap: (x: number, y: number) => string;
-export declare const urlIsLong: (url: string) => boolean;
-export declare const GenNonDuplicateID: () => string;
-export declare const compareVersion: (version1: string, version2: string) => 0 | 1 | -1;
-export declare const getPagePerformance: () => any;
-export declare const sleep: (ms: any) => Promise<unknown>;
