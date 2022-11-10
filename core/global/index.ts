81cea0b40ca658f349a31a3bca776bbba170ce44
liuqi <70128222+l1uqi@users.noreply.github.com>
2022-11-10 15:19:00 +0800

    fix(options): setconfig  上一次值未能保存

--- core/global/index.ts
+++ core/global/index.ts
@@ -5,17 +5,18 @@ import { compareVersion, getCache, setCache } from "../utils/index";
 import { setTrackV3Directives } from "./directives_v3";
 import { sessionStoreEnum } from "../enum";
 
-export function initGlobalFun(FSTracker) {
-  FSTracker.prototype.setConfig = function (options) {
+export function initGlobalFun(SJTracker) {
+  SJTracker.prototype.setConfig = function (options) {
     options = Object.assign({}, this._options, options);
+    this._options = options;
     setCache(sessionStoreEnum.OPSIONS, options);
   };
 
-  FSTracker.prototype.sendTracker = function (data = {}) {
+  SJTracker.prototype.sendTracker = function (data = {}) {
     sendTracker(this._options, data);
   };
 
-  FSTracker.prototype.initDirectives = function (app) {
+  SJTracker.prototype.initDirectives = function (app) {
     if (app.version && compareVersion("3.0.0", app.version) > 0) {
       setTrackV2Directives(app);
     } else {
@@ -23,15 +24,17 @@ export function initGlobalFun(FSTracker) {
     }
   };
 
-  FSTracker.prototype.registerVueRouterEvent = function (router, cb) {
+  SJTracker.prototype.registerVueRouterEvent = function (router, cb) {
     setVueRouterEvent(router, this._options, cb);
   };
 
-  FSTracker.prototype.getUUID = function () {
+  SJTracker.prototype.getUUID = function () {
     return getCache(sessionStoreEnum.UUID);
   };
 
-  FSTracker.prototype.registerErrorEvent = function (cb) {
+  SJTracker.prototype.registerErrorEvent = function (cb) {
     initErrorEvent(cb);
   };
+
+
 }
